dfac13ef142c08f0060f7e49ac6cdcd5b3e5d43f
Johan Ostrand <muztanger@gmail.com>
2019-11-14 20:11:18 +0100

    Moved a couple of functions to a common class.

--- Advent of Code 2015/Day10.cs
+++ Advent of Code 2015/Day10.cs
@@ -12,37 +12,14 @@ namespace Advent_of_Code_2015
     [TestClass]
     public class Day10
     {
-
-
-        // https://stackoverflow.com/questions/228038/best-way-to-reverse-a-string?page=1&tab=votes#tab-top
-        public static string Reverse(string s)
-        {
-            char[] charArray = s.ToCharArray();
-            Array.Reverse(charArray);
-            return new string(charArray);
-        }
-
-
-
-        static string exampleString = @"";
-
         [TestMethod]
         public void Part1Examples()
         {
-            /*
-             * 
-    1 becomes 11 (1 copy of digit 1).
-    11 becomes 21 (2 copies of digit 1).
-    21 becomes 1211 (one 2 followed by one 1).
-    1211 becomes 111221 (one 1, one 2, and two 1s).
-    111221 becomes 312211 (three 1s, two 2s, and one 1).
-*/
             Assert.AreEqual("11", lookAndSay("1"));
             Assert.AreEqual("21", lookAndSay("11"));
             Assert.AreEqual("1211", lookAndSay("21"));
             Assert.AreEqual("111221", lookAndSay("1211"));
             Assert.AreEqual("312211", lookAndSay("111221"));
-
         }
 
         private string lookAndSay(string v)
@@ -65,7 +42,7 @@ namespace Advent_of_Code_2015
                 last = x;
             }
             result.Append($"{last}{count}");
-            return Reverse(result.ToString());
+            return Common.Reverse(result.ToString());
         }
 
 
